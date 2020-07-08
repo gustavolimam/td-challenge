ef201b90945eefb92dd3c859c6480b758b3e0501
Gustavo Lima <gustavolimash@gmail.com>
2020-07-07 23:20:04 -0300

    get port

--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ app.use(express.json());
 
 app.use(require("./routes"));
 
-server.listen(3000, () => {
+server.listen((process.env.PORT || 5000), () => {
   console.log("Server started on port 3000");
 });
